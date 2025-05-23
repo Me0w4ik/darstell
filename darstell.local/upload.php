0a14f8dc9e0a8b039281ac736384f0c3d9040c2c
Me0w4ik <fervaster@gmail.com>
2025-05-23 11:51:46 +0300

    Update upload.php

--- darstell.local/upload.php
+++ darstell.local/upload.php
@@ -6,6 +6,42 @@ require_once __DIR__ . "/src/helpers.php";
 require "src/addimg.php";
 
 $connect = getDB();
+
+$search = isset($_GET['search']) ? $_GET['search'] : '';
+
+if ($search) {
+  $search = mysqli_real_escape_string($connect, $search); // Экранирование для безопасности
+  header('Location: /search.php?search=' . urlencode($search));
+  exit();
+}
+
+$idUser = $_SESSION['user']['id'];
+
+if ($idUser == ''){
+  header(header: "Location: /login.php");
+  exit();
+}
+
+$sql = "SELECT * FROM users WHERE id = '$idUser'";
+
+$result = mysqli_query($connect, $sql);
+$result = mysqli_fetch_all(result: $result);
+
+$nik;
+$description;
+$avatar;
+
+foreach($result as $item){
+  $nik = $item[1];
+  $description = $item[4];
+  $avatar = $item[5];
+}
+
+if ($nik == ''){
+  header("Location: /src/ban.php");
+  exit();
+}
+
 if (!$connect) {
     die("Ошибка подключения к базе данных: " . mysqli_connect_error());
 }
@@ -46,7 +82,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['image'])) {
         if (imageSecurity($image)) {
             loadimage($idUser, $image, $name, $description, $tags);
         } else {
-            echo("errorsecurity"); // Безопасность аватара не прошла проверку.
+            echo("errorsecurity");
             exit();
         }
     }
@@ -100,37 +136,37 @@ if (isset($data['addimage'])) {
 }
 ?>
 
-
-
-
-
 <!DOCTYPE html>
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="css/upload.css">
-    <link rel="icon" href="icon/DarstellIcon.png" type="image/png">
-    <title>upload - Darstell</title>
+    <link rel="icon" href="icon/Darstell.ico" type="image/x-icon">
+    <title>Upload - Darstell</title>
 </head>
 <body>
-<div class="header">
+  <header>
     <div class="container-menu">
       <div class="header-line">
         <div class="logo-menu">
-          <div class="header-logo">
-            <a href="/">
+          <div class="header-logo" href="@">
+            <a href="/" style="display: flex;">
               <img header-logo src="icon/darstell.svg">
             </a>
           </div>
         </div>
-        <div class="poisk-c">
-          <input id="input" type="text" placeholder="Найти" class="poisk">
-          <img src="icon/lupa.svg" alt="Искать" class="poisk-icon">
-        </div>
+        <form class="poisk-c" method="GET" action="">
+          <input required autocomplete="off" type="text" name="search" placeholder="Найти" class="poisk" id="poiskc">
+          <button class="buttonpoisk" type="submit">
+            <svg class="poisk-icon" viewBox="0 0 19 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M8.07131 0.00390317C9.34289 -0.0350485 10.5939 0.222945 11.7266 0.742537C12.2988 1.00501 12.3797 1.74885 11.9395 2.19899L11.9183 2.22073C11.5991 2.54715 11.1024 2.60774 10.6811 2.43193C9.94233 2.12368 9.141 1.96 8.32229 1.95996C6.72684 1.95996 5.19654 2.58028 4.06838 3.68359C2.94026 4.78689 2.30667 6.28346 2.30667 7.84375C2.30672 8.61621 2.46241 9.38106 2.76467 10.0947C3.06696 10.8084 3.50989 11.4567 4.06838 12.0029C4.62689 12.5491 5.28985 12.9827 6.01956 13.2783C6.74941 13.574 7.5323 13.7266 8.32229 13.7266C9.11216 13.7265 9.8943 13.5739 10.624 13.2783C11.3538 12.9827 12.0167 12.5492 12.5752 12.0029C13.1338 11.4566 13.5776 10.8085 13.8799 10.0947C14.1821 9.38109 14.3379 8.61617 14.3379 7.84375C14.3379 7.07579 14.1834 6.32359 13.8924 5.62599C13.7197 5.21184 13.7726 4.72434 14.0862 4.40342L14.1344 4.35419C14.5752 3.90308 15.3228 3.9682 15.5942 4.53761C15.9774 5.34148 16.222 6.20967 16.3086 7.1084C16.4712 8.79644 16.0693 10.482 15.1772 11.9175C14.9067 12.3526 14.9366 12.9265 15.3029 13.2847L18.3877 16.3018C18.4861 16.3914 18.5653 16.5 18.6201 16.6201C18.6749 16.7403 18.7047 16.8703 18.7071 17.002C18.7094 17.1336 18.6842 17.2646 18.6338 17.3867C18.5834 17.5087 18.5083 17.6198 18.4131 17.7129C18.318 17.8058 18.2048 17.8794 18.0801 17.9287C17.9553 17.978 17.8211 18.0023 17.6865 18C17.5522 17.9976 17.4197 17.9685 17.2969 17.915C17.1739 17.8614 17.0625 17.7838 16.9707 17.6875L13.858 14.6433C13.5037 14.2967 12.952 14.2682 12.5274 14.5238C11.051 15.4127 9.31197 15.8138 7.57034 15.6533C5.53917 15.4661 3.65724 14.5295 2.30764 13.0332C0.958017 11.5367 0.241829 9.59306 0.305689 7.59863C0.369567 5.6042 1.20867 3.70883 2.65139 2.29785C4.09408 0.886938 6.03206 0.0664495 8.07131 0.00390317Z" fill-opacity="0.8" />
+            </svg>
+          </button>
+        </form>
         <div class="menu-icon">
           <a href="upload.php" class="menu-icon-upload">
-            <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg width="24" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M10.8918 14.2712V1.41214M10.8918 1.41214L13.8593 4.8742M10.8918 1.41214L7.92432 4.8742" stroke-opacity="0.8" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
               <path d="M6.93495 21.1953H14.8482C17.6456 21.1953 19.0452 21.1953 19.9137 20.3269C20.7832 19.4564 20.7832 18.0587 20.7832 15.2604V14.2712C20.7832 11.4739 20.7832 10.0752 19.9137 9.20574C19.154 8.44607 17.9888 8.35012 15.8374 8.33825M5.9458 8.33825C3.79437 8.35012 2.62915 8.44607 1.86947 9.20574C1 10.0752 1 11.4739 1 14.2712V15.2604C1 18.0587 1 19.4574 1.86947 20.3269C2.16622 20.6236 2.52429 20.8185 2.97832 20.9471" stroke-width="1.5" stroke-linecap="round" />
             </svg>
@@ -143,44 +179,14 @@ if (isset($data['addimage'])) {
           </a>
         </div>
       </div>
-      <div class="parent-container">
-        <div class="palka"></div>
-      </div>
-      <div class="header-line">
-        <div class="battons">
-          <a class="batton-oboi" href="@">Обои</a>
-          <div class="general-palk"></div>
-          <a class="batton-priroda" href="@">Природа</a>
-          <div class="general-palk"></div>
-          <a class="batton-3D" href="@">3D</a>
-          <div class="general-palk"></div>
-          <a class="batton-textura" href="@">4K</a>
-          <div class="general-palk"></div>
-          <a class="batton-intereri" href="@">Интерьеры</a>
-          <div class="general-palk"></div>
-          <a class="batton-4k" href="@">Текстуры</a>
-          <div class="general-palk"></div>
-          <a class="batton-filmi" href="@">Фильм</a>
-          <div class="general-palk"></div>
-          <a class="batton-ludi" href="@">Люди</a>
-          <div class="general-palk"></div>
-          <a class="batton-zoo" href="@">Животные</a>
-          <div class="general-palk"></div>
-          <a class="batton-game" href="@">Игры</a>
-          <div class="general-palk"></div>
-          <a class="batton-moda" href="@">Мода</a>
-          <div class="general-palk"></div>
-          <a class="batton-eda" href="@">Еда</a>
-        </div>
-      </div>
     </div>
-  </div>
+  </header>
   <main>
-    <div class="container-menu">
+    <div class="container-menu2">
         <div class="uploadbackground">
             <form class="form" enctype="multipart/form-data" method="post" id="formuploadimg">
                   <div class="upload-img">
-                      <input id="uploadimg" type="file" accept=".jpg, .jpeg, .png" name="image">
+                      <input required id="uploadimg" type="file" accept=".jpg, .jpeg, .png" name="image">
                       <label for="uploadimg" class="upload-imglabel">
                         <div id="preview"></div>
                         <div class="infocub">
@@ -224,9 +230,7 @@ if (isset($data['addimage'])) {
                               </defs>
                             </svg>
                           </div>
-                          <div>
-                            <p class="upload-img-text2">Нажми, что бы вставить изображение</p>
-                          </div>
+                          <p class="upload-img-text2">Нажми, что бы вставить изображение</p>
                         </div>
                       </label>
                   </div>
@@ -247,20 +251,9 @@ if (isset($data['addimage'])) {
                     </div>
                     <div class="niz">
                         <div class="tagi">
-                            <a class="tags">Обои</a>
-                            <a class="tags">Природа</a>
-                            <a class="tags">3D</a>
-                            <a class="tags">4К</a>
-                            <a class="tags">Интерьеры</a>
-                            <a class="tags">Текстуры</a>
-                            <a class="tags">Фильм</a>
-                            <a class="tags">Люди</a>
-                            <a class="tags">Животные</a>
-                            <a class="tags">Игры</a>
-                            <a class="tags">Мода</a>
-                            <a class="tags">Еда</a>
+                            <a class="tags">Напишите теги через запятую (пример: обои, интерьеры, мода)</a>
                         </div>
-                        <button type="submit" name="addimage">Загрузить</button>
+                        <button class="addimage" type="submit" name="addimage">Загрузить</button>
                     </div>
                 </div>
             </form>
@@ -305,7 +298,7 @@ $("#formuploadimg").on("submit", function(e) {
       if (data.includes('filewin')) {
         $('#filewin').addClass('show appear');
         $('.profile-avatar, .arrow.dison, .submitava.dison').addClass('disoff').removeClass('dison');
-        window.location.href = 'profile.php';
+        window.location.href = 'upload.php';
       } else if (data.includes('errorsecurity')) {
         $('#errorsecurity').addClass('show appear');
       }
